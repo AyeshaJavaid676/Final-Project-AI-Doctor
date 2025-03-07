ec88eeab27f2341b322257a3a17430602ae69865
AyeshaJavaid676 <a.javaid100@gmail.com>
2025-03-07 21:58:31 +0500

    Add files via upload

---
+++ gradio_app.py
@@ -0,0 +1,56 @@
+# if you dont use pipenv uncomment the following:
+# from dotenv import load_dotenv
+# load_dotenv()
+
+#VoiceBot UI with Gradio
+import os
+import gradio as gr
+
+from brain_of_the_doctor import encode_image, analyze_image_with_query
+from voice_of_the_patient import record_audio, transcribe_with_groq
+from voice_of_the_doctor import text_to_speech_with_gtts, text_to_speech_with_elevenlabs
+
+#load_dotenv()
+
+system_prompt=""" You have to act like a professional doctor.
+            What's in this image?. Do you find anything wrong with it medically? 
+            If you make a differential, suggest some remedies for them.  Also always answer as if you are answering to a real person.
+            Donot say 'In the image I see' but say 'With what I see, I think you have ....'
+            Dont respond as an AI model in markdown, your answer should mimic that of an actual doctor not an AI bot, 
+            No preamble, start your answer right away please Fist tell the patient about disease, or issues then tell some remedies or steps to overcome it with heading. Then write in Urdu the steps in bullet points. and then tell whether to visit the doctor or not and tell the seriousness of condition. Also tell natural ways and foods he should eat for healthy lifestyle.
+"""
+
+
+def process_inputs(audio_filepath, image_filepath):
+    speech_to_text_output = transcribe_with_groq(GROQ_API_KEY=os.environ.get("GROQ_API_KEY"), 
+                                                 audio_filepath=audio_filepath,
+                                                 stt_model="whisper-large-v3")
+
+    # Handle the image input
+    if image_filepath:
+        doctor_response = analyze_image_with_query(query=system_prompt+speech_to_text_output, encoded_image=encode_image(image_filepath), model="llama-3.2-11b-vision-preview")
+    else:
+        doctor_response = "No image provided for me to analyze"
+
+    voice_of_doctor = text_to_speech_with_elevenlabs(input_text=doctor_response, output_filepath="finall.mp3") 
+
+    return speech_to_text_output, doctor_response, voice_of_doctor
+
+
+# Create the interface
+iface = gr.Interface(
+    fn=process_inputs,
+    inputs=[
+        gr.Audio(sources=["microphone"], type="filepath"),
+        gr.Image(type="filepath")
+    ],
+    outputs=[
+        gr.Textbox(label="Speech to Text"),
+        gr.Textbox(label="Doctor's Response"),
+        gr.Audio("Temp.mp3")
+    ],
+    title="DocBot: Your Virtual Health Assistant"
+)
+
+iface.launch(debug=True,share=True)
+
